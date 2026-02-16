94d3078076269895de0f860568b87485f843c95f
patrik <smrzo@ASUS_PATRIK>
2026-02-16 16:30:57 +0100

    hmmm jeden z takych prvych commitov lebo chcem ale uz viem konecne co ako robit

---
+++ A&P SmartHub/Graphics/Additional/CustomUpperBar.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace A_P_SmartHub.Graphics.Additional
+{
+    /// <summary>
+    /// Interaction logic for CustomUpperBar.xaml
+    /// </summary>
+    public partial class CustomUpperBar : UserControl
+    {
+        public CustomUpperBar()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Window.GetWindow(this).Close();
+        }
+
+        private void MaxRestoreButton_Click(object sender, RoutedEventArgs e)
+        {
+            var okno = Window.GetWindow(this);
+            if (okno.WindowState == WindowState.Normal)
+                okno.WindowState = WindowState.Maximized;
+            else
+                okno.WindowState = WindowState.Normal;
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Window.GetWindow(this).WindowState = WindowState.Minimized;
+        }
+    }
+}
