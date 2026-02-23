51bd7636606eb05ba4b71f4cc6e92b9e17667203
Alex Ozaniak <alexozaniakk@gmail.com>
2026-02-23 23:03:10 +0100

    registracia na 70% done treba pridat ukladanie atd zacal som aj verifikaciu robit uz to posiela mail aj kontrola ide idem spat :)) KONECNE

--- A&P SmartHub/Graphics/MainGrap/Registration/Register.xaml.cs
+++ A&P SmartHub/Graphics/MainGrap/Registration/Register.xaml.cs
@@ -1,6 +1,12 @@
 ﻿using A_P_SmartHub.Graphics.Additional;
+using A_P_SmartHub.Graphics.MainGrap.Registration;
+using BCrypt.Net;
+using Microsoft.Data.Sqlite;
+using Microsoft.Win32;
+using SQLitePCL;
 using System;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,12 +17,16 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using VerificationCodeWindow = A_P_SmartHub.Graphics.Additional.VerificationCodeWindow;
 
 namespace A_P_SmartHub.Graphics.MainGrap
 {
     /// <summary>
     /// Interaction logic for Register.xaml
     /// </summary>
+    /// 
+
+        
     public partial class Register : UserControl
     {
         public Register()
@@ -24,15 +34,56 @@ namespace A_P_SmartHub.Graphics.MainGrap
             InitializeComponent();
         }
 
+
+        public string Mail { get; set; }
+        private string Password { get; set; }
+        private string PassHash { get; set; }
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            var mainWindow = Window.GetWindow(this) as MainWindow;
+            
+            smtpClientMail smtpClientMail = new smtpClientMail();
+            VerificationCodeWindow verificationCode  = new VerificationCodeWindow();
 
-            if (mainWindow != null)
+
+
+
+            if (Passw1.Text != Passw2.Text)
+            {
+                MessageBox.Show("Password do not match");
+                return;
+            }
+            else if (Passw1.Text.Length < 8)
+            {
+                MessageBox.Show("This password is too weak, please use password with 8 or more chars");
+                return;
+            }
+            else if (!EmailRegWind.Text.Contains("@"))
+            {
+                MessageBox.Show("Invalid Mail format, maybe you're missing '@'");
+                return;
+            }
+            else
             {
 
-                mainWindow.MainDisplay.Content = new VerificationCodeWindow();
+                Mail = EmailRegWind.Text;
+                Password = Passw1.Text;
+                PassHash = BCrypt.Net.BCrypt.EnhancedHashPassword(Password);
+                bool uncrypPass = BCrypt.Net.BCrypt.EnhancedVerify(Password, PassHash);
+                var mainWindow = Window.GetWindow(this) as MainWindow;
+                smtpClientMail.SendMail(verificationCode, this);
+                mainWindow.MainDisplay.Content = verificationCode;
             }
+            
+
+        }
+     
+        
+
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+           //
         }
     }
 }
