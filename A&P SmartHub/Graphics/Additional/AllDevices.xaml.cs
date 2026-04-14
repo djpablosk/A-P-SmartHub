196c7bd2ad2a463bd5a83ebbb30f885a684514d1
patrik <smrzo@ASUS_PATRIK>
2026-04-14 16:40:58 +0200

    new dashboard layout new animation in dashboard new settings screen new menu screen allDevice screen is done without !animation! fixed some bugs

--- A&P SmartHub/Graphics/Additional/AllDevices.xaml.cs
+++ A&P SmartHub/Graphics/Additional/AllDevices.xaml.cs
@@ -21,6 +21,32 @@ namespace A_P_SmartHub.Graphics.Additional
         public AllDevices()
         {
             InitializeComponent();
+            var myDevices = new List<SmartDevice>
+{
+    new SmartDevice { Name = "Main Light" },
+    new SmartDevice { Name = "Thermostat" },
+    new SmartDevice { Name = "TV Living Room" },
+    new SmartDevice { Name = "Led Strip" },
+    new SmartDevice { Name = "Humidifier" },
+    new SmartDevice { Name = "PC Station" },
+    new SmartDevice { Name = "Camera 1" },
+    new SmartDevice { Name = "Router" },
+     new SmartDevice { Name = "Humidifier" },
+    new SmartDevice { Name = "PC Station" },
+    new SmartDevice { Name = "Camera 1" },
+    new SmartDevice { Name = "Router" },
+     new SmartDevice { Name = "Humidifier" },
+    new SmartDevice { Name = "PC Station" },
+    new SmartDevice { Name = "Camera 1" },
+    new SmartDevice { Name = "Router" }
+};
+
+            // A tu to pripojíme na ten náš XAML zoznam
+            DeviceList.ItemsSource = myDevices;
+        }
+        public class SmartDevice
+        {
+            public string Name { get; set; }
         }
     }
 }
