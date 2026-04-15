a41f8729712a62a3211778439604143a9d58b158
patrik <smrzo@ASUS_PATRIK>
2026-04-15 09:08:25 +0200

    new function reset password new 3 screens for reset password

---
+++ A&P SmartHub/Graphics/Additional/ForgotPassword/NewPassword.xaml.cs
@@ -0,0 +1,39 @@
+﻿using A_P_SmartHub.Graphics.Login;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace A_P_SmartHub.Graphics.Additional.ForgotPassword
+{
+    /// <summary>
+    /// Interaction logic for NewPassword.xaml
+    /// </summary>
+    public partial class NewPassword : UserControl
+    {
+        public NewPassword()
+        {
+            InitializeComponent();
+        }
+
+        private void ResetPassword_Click(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = Window.GetWindow(this) as MainWindow;
+
+
+            if (mainWindow != null)
+            {
+
+                mainWindow.SlideViewTransition(new A_P_SmartHub.Graphics.Login.Login(), true);
+            }
+        }
+    }
+}
