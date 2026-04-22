19d4b278cf1f1f3fd04df0c23ad6a5d00ed4c576
Alex Ozaniak <alexozaniakk@gmail.com>
2026-04-22 10:10:03 +0200

    trallalero trallala

--- A&P SmartHub/Graphics/Additional/ForgotPassword/MailScreen.xaml.cs
+++ A&P SmartHub/Graphics/Additional/ForgotPassword/MailScreen.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using A_P_SmartHub.Databazicky;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -18,29 +19,56 @@ namespace A_P_SmartHub.Graphics.Additional.ForgotPassword
     /// </summary>
     public partial class MailScreen : UserControl
     {
+       
+        
         public MailScreen()
         {
+            
             InitializeComponent();
         }
+       public string ForgotMail {  get; set; }
 
         private void SendCode_Click(object sender, RoutedEventArgs e)
         {
-            // Find the parent NewPasswordScreen in the visual tree and request it to show the CodeScreen
-            DependencyObject parent = this;
-            while (parent != null)
+            NewPassword password = new NewPassword();
+           
+
+            ForgotMail = forgotPassMail.Text;
+            
+            smtpClientMail smtpClientMail = new smtpClientMail();
+            SQLITE_Users users = new SQLITE_Users();
+            CodeScreen codeScreen   = new CodeScreen();
+            
+            string exists = users.GetUserId(forgotPassMail.Text);
+
+            if (exists.IsWhiteSpace())
             {
-                parent = VisualTreeHelper.GetParent(parent);
-                if (parent is NewPasswordScreen newPasswordScreen)
+                MessageBox.Show("This Mail Was Not Found In Our System Maybe You Want To Try Another Mail");
+            }
+            else
+            {
+
+
+                // Find the parent NewPasswordScreen in the visual tree and request it to show the CodeScreen
+                DependencyObject parent = this;
+                while (parent != null)
                 {
-                    newPasswordScreen.ShowCodeScreen();
-                    break;
+                    parent = VisualTreeHelper.GetParent(parent);
+                    if (parent is NewPasswordScreen newPasswordScreen)
+                    {
+                        
+                        newPasswordScreen.ShowCodeScreen(codeScreen); // ✅ SAME OBJECT
+                        break;
+                    }
                 }
+                smtpClientMail.SendCode(codeScreen, this);
+
+
+
             }
         }
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
 
-        }
+       
     }
 }
