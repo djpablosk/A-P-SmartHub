227a85cd9bb29cb4f48916174d5ab645c2035f0d
patrik <smrzo@ASUS_PATRIK>
2026-04-25 14:15:39 +0200

    loading on dashboard is in progress new full ai code but i want to remake it because i dont like it and i want to know how to do

--- A&P SmartHub/Graphics/Additional/HomePage.xaml.cs
+++ A&P SmartHub/Graphics/Additional/HomePage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using A_P_SmartHub.Graphics.Login;
 using A_P_SmartHub.Graphics.MainGrap;
+using A_P_SmartHub.Type_devices_with_graphics.graphicsForDevicesType;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,6 +13,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using A_P_SmartHub.Graphics.Additional;
+using A_P_SmartHub.Type_devices_with_graphics;
+using A_P_SmartHub.Type_devices_with_graphics.graphicsForDevicesType;
+
 using static A_P_SmartHub.Graphics.MainGrap.Dashboard.MainDashboard;
 
 namespace A_P_SmartHub.Graphics.Additional
@@ -19,26 +25,29 @@ namespace A_P_SmartHub.Graphics.Additional
     /// <summary>
     /// Interaction logic for HomePage.xaml
     /// </summary>
+    /// 
+   
     public partial class HomePage : UserControl
     {
+        public ObservableCollection<DeviceType> MyDevices { get; set; }
         public HomePage()
         {
             InitializeComponent();
-            var myDevices = new List<SmartDevice>
-    {
-        new SmartDevice { Name = "Main Light" },
-        new SmartDevice { Name = "Thermostat" },
-        new SmartDevice { Name = "TV Living Room" },
-        new SmartDevice { Name = "Led Strip" },
-        new SmartDevice { Name = "Humidifier" },
-        new SmartDevice { Name = "PC Station" },
-        new SmartDevice { Name = "Camera 1" },
-        new SmartDevice { Name = "Router" }
-    };
 
-            // A tu to pripojíme na ten náš XAML zoznam
-            DeviceList.ItemsSource = myDevices;
+            MyDevices = new ObservableCollection<DeviceType>();
+
+            DeviceList.ItemsSource = MyDevices;
+            LoadTestData();
         }
+        private void LoadTestData()
+        {
+            // Vytvárame nové zariadenia a hádžeme ich do zoznamu
+            MyDevices.Add(new DeviceType { ID = 1, Name = "Stolná Lampa", Type = DeviceTypeEnum.Lights });
+            MyDevices.Add(new DeviceType { ID = 2, Name = "Kuchynský Pás", Type = DeviceTypeEnum.Lights });
+            MyDevices.Add(new DeviceType { ID = 3, Name = "Termostat Obývačka", Type = DeviceTypeEnum.Climates });
+            MyDevices.Add(new DeviceType { ID = 4, Name = "Kávovar", Type = DeviceTypeEnum.Toggles });
+        }
+
         public class SmartDevice
         {
             public string Name { get; set; }
@@ -67,5 +76,25 @@ namespace A_P_SmartHub.Graphics.Additional
                 mainWindow.SlideViewTransition(new A_P_SmartHub.Graphics.Login.Login(), true);
             }
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button stlaceneButton = sender as Button;
+            DeviceType stlaceneDevice = stlaceneButton.DataContext as DeviceType;
+
+            if (stlaceneDevice != null)
+            {
+                switch (stlaceneDevice.Type)
+                {
+                    case DeviceTypeEnum.Lights:
+                        // Oprava chyby CS1503: Posielame presne ten typ, ktorý okno čaká
+                        var lightWindow = new LightTemplate(stlaceneDevice);
+                        PopupContent.Content = lightWindow;
+                        PopupOverlay.Visibility = Visibility.Visible;
+                        break;
+                }
+            }
+        }
+
     }
 }
