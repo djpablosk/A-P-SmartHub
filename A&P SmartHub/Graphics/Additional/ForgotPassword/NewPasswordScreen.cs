0fb51ea999470a2b0f9192c6eabe2207728d1ad6
patrik <smrzo@ASUS_PATRIK>
2026-04-28 00:02:51 +0200

    all textboxes are fixed fixed some bugs with reset password

---
+++ A&P SmartHub/Graphics/Additional/ForgotPassword/NewPasswordScreen.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace A_P_SmartHub.Graphics.Additional.ForgotPassword
+{
+    // Simple wrapper class so other code can reference NewPasswordScreen (PascalCase)
+    // while XAML class remains named newpasswordScreen
+    public class NewPasswordScreen : newpasswordScreen
+    {
+    }
+}
