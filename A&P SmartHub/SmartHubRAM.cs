52b762ec03ccd1ccac161a81eba2458da919d0f4
patrik <smrzo@ASUS_PATRIK>
2026-05-16 14:23:00 +0200

    new system of own RAM when is app not closet all states is saved bug fixes in climate

---
+++ A&P SmartHub/SmartHubRAM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace A_P_SmartHub
+{
+    public static class SmartHubRAM
+    {
+        //climate
+        public static double SavecurrentTemperatureClimate = 25.0;
+        public static string SavecurrentstatClimate = "AUTO";
+
+        //light
+        public static double SavecurrentBrightnessLight = 0;
+
+        //media
+        public static double SavecurrentVolumeLevel = 0;
+    }
+}
--- A&P SmartHub/Type devices with graphics/graphicsForDevicesType/LightTemplate.xaml.cs
+++ A&P SmartHub/Type devices with graphics/graphicsForDevicesType/LightTemplate.xaml.cs
@@ -19,18 +19,16 @@ namespace A_P_SmartHub.Type_devices_with_graphics.graphicsForDevicesType
     /// </summary>
     public partial class LightTemplate : UserControl
     {
-        // Konštruktor, ktorý prijíma dáta
-        // Konštruktor teraz presne vie, čo je DeviceType
+        
         public LightTemplate(DeviceType device)
         {
             InitializeComponent();
             this.DataContext = device;
+            this.Loaded += LightControl_Loaded;
         }
 
-        // Metóda pre krížik (zavretie)
         private void CloseButton_Click(object sender, RoutedEventArgs e)
         {
-            // Hľadáme PopupOverlay na HomePage, aby sme ho skryli
             DependencyObject parent = VisualTreeHelper.GetParent(this);
             while (parent != null && !(parent is Grid && ((Grid)parent).Name == "PopupOverlay"))
             {
@@ -46,6 +44,31 @@ namespace A_P_SmartHub.Type_devices_with_graphics.graphicsForDevicesType
         private void BrightnessSlider_MouseWheel(object sender, MouseWheelEventArgs e)
         {
            BrightnessSlider.Value -= e.Delta > 0 ? 1 : -1;
+        }
+
+       
+
+        private void BrightnessSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            SmartHubRAM.SavecurrentBrightnessLight = e.NewValue;
+
+            if (BrightnessText != null)
+            {
+                BrightnessText.Text = $"{Math.Round(e.NewValue)}%";
+            }
+        }
+        private void LightControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            BrightnessSlider.ValueChanged -= BrightnessSlider_ValueChanged;
+
+            BrightnessSlider.Value = SmartHubRAM.SavecurrentBrightnessLight;
+
+            if (BrightnessText != null)
+            {
+                BrightnessText.Text = $"{Math.Round(SmartHubRAM.SavecurrentBrightnessLight)}%";
+            }
+
+            BrightnessSlider.ValueChanged += BrightnessSlider_ValueChanged;
         }
     }
 }
